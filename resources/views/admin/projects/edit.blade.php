64611501fc2e198ac682d602a76ac47528a4aa37
UmbertoSerino <serumberto@gmail.com>
2024-02-12 22:10:54 +0100

    create edit

--- resources/views/admin/projects/edit.blade.php
+++ resources/views/admin/projects/edit.blade.php
@@ -21,30 +21,32 @@
             </div>
         @endif
 
-            <form action="{{ route('admin.projects.store') }}" method="POST">
+            <form action="{{ route('admin.projects.update', $project) }}" method="POST">
                 @csrf
+                @method('PUT')
 
                 <div class="mb-3 input-group">
                     <label for="title" class="input-group-text">Titolo:</label>
-                    <input class="form-control" type="text" name="title" id="title" value="{{ old('title') }}">
+                    <input class="form-control" type="text" name="title" id="title" value="{{ old('title', $project->title) }}">
                 </div>
                 <div class="mb-3 input-group">
                     <label for="date" class="input-group-text">Data:</label>
-                    <input class="form-control" type="date" name="date" id="date" value="{{ old('date') }}">
+                    <input class="form-control" type="date" name="date" id="date" value="{{ old('date', $project->data) }}">
                 </div>
                 {{-- modificarlo con checkbox --}}
                 <div class="mb-3 input-group">
                     <label for="complete" class="input-group-text">Comletato?:</label>
-                    <input class="form-control" type="text" name="complete" id="complete" value="{{ old('complete') }}">
+                    <input class="form-control" type="text" name="complete" id="complete" value="{{ old('complete', $project->complete) }}">
                 </div>
                 <div class="mb-3 input-group">
                     <label for="description" class="input-group-text">Descrizione:</label>
-                    <textarea class="form-control"  name="description" id="description" cols="40" rows="10">{{ old('description')  }}</textarea>
+                    <textarea class="form-control"  name="description" id="description" cols="40" rows="10">{{ old('description',$project->description)  }}</textarea>
                 </div>
                 <div class="mb-3 input-group">
-                    <button class="btn btn-success">
-                        Crea
-                    </button>
+                    <a href="{{ route('admin.projects.show', $project) }}" class="m-2">
+                        <button class="btn btn-success inline-block">Modifica</button>
+                    </a>
+
                     <a href="{{ route('admin.projects.index') }}" class="btn btn-primary">Torna indietro</a>
 
                 </div>
