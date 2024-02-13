4a1525173a4a21a91f632a8b1f9b1e4fa1c5bb42
UmbertoSerino <serumberto@gmail.com>
2024-02-13 17:32:54 +0100

    created table and model soft delete

--- app/Models/Project.php
+++ app/Models/Project.php
@@ -4,10 +4,12 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Project extends Model
 {
     use HasFactory;
+    use SoftDeletes;
     public $table = 'projects';
     protected $fillable = ['title', 'description', 'date', 'complete'];
 }
